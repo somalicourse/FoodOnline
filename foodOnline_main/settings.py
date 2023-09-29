d024e407739e97dda1af04a2db9be064dd8146b1
somalicourse <cabdinajiibrcabdirashiid@gmail.com>
2023-09-29 18:42:56 +0300

    You can registerUser Now

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','192.168.1.109']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -83,7 +84,7 @@ DATABASES = {
         'HOST':config('DB_HOST'),
     }
 }
-
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -125,7 +126,20 @@ STATICFILES_DIRS = [
     'foodOnline_main/static'
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_STATIC = BASE_DIR/'media'
+
+# Media Files configuration
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+  
+}
