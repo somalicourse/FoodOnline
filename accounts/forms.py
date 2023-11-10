3c2d5b6ebc501fc66c18202451b99700e932bc15
somalicourse <cabdinajiibrcabdirashiid@gmail.com>
2023-11-10 20:25:28 +0300

    restaurent profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import User
+from .models import User , UserProfile
+from .validators import allow_only_images_validator
+
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
@@ -18,3 +20,17 @@ class UserForm(forms.ModelForm):
             raise forms.ValidationError(
                 'Password Does Not Match!'
             )
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'starttyping..','required':'required'}))
+    profile_picture = forms.FileField(validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(validators=[allow_only_images_validator])
+    
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture','cover_photo','address','country','state','city','pin_code','latitude','longitude']
+
+    def __init__(self, *args,**kwargs):
+        super(UserProfileForm,self).__init__(*args,**kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
---
+++ accounts/validators.py
@@ -0,0 +1,9 @@
+from django.core.exceptions import ValidationError
+import os
+
+def allow_only_images_validator(value):
+    ext = os.path.splitext(value.name)[1]
+    print(ext)
+    valid_extensions = ['.png','jpg','jpng']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension: '+str(valid_extensions))
